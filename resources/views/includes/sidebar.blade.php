7aaa640f843760a65929821f87b52b5d18a606b6
Teran Stone <tstone@eoj.com.jm>
2018-07-13 17:41:07 -0500

    minor changes to improve ie ui views

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,4 +1,4 @@
-<nav class="col-md-2 d-none d-md-block bg-light sidebar navbar-fixed-top">
+<nav class="col-md-2 d-none d-md-block bg-light sidebar navbar-fixed-top" id="sidebar">
 	<div style="padding-bottom: 10px;border-bottom: 1px solid red;padding-right: 5px;padding-left: 5px;">
 		<form action="{{route('login')}}" method="post" id="searcher">
 			<input class="form-control" type="text" name="searchapp" id="searchapp" placeholder="Search All Apps" style="font-size: 13px;margin-top: 10px;height: 30px;">
@@ -6,9 +6,9 @@
 		</form>
 	</div>
   <div class="sidebar-sticky">
-	<ul class="nav flex-column">     
-<!--Print different user names-->	
-	@foreach($sidebar as $side)		
+	<ul class="nav flex-column">
+<!--Print different user names-->
+	@foreach($sidebar as $side)
 	  <li class="nav-item slideout"  data-name="{{$side->user_id}}">
 		<a class="nav-link arrow" href="{{'#'.$side->user_id}}">
 		  <svg xmlns="http://www.w3.org/2000/svg" x="0" y="10px" width="24" height="24" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,22 +14,22 @@
 				<div class="container">
 					<h1>Welcome To Our Site</h1>
 					<p class="lead">
-						Welcome to the Electoral Office Software Hub Application. 
+						Welcome to the Electoral Office Software Hub Application.
 					</p>
 					<p style="font-size: 14px;">
 						<span  class="arrow" style="font-size:  30px; top: 4px; position: relative;">&#x21FD;</span>
-						Select A Name To See The Associated Applications. 
+						Select A Name To See The Associated Applications.
 					</p>
 				</div>
 			</div>
 			<div class="row posts">
 			<div class="col-md-6 appscontainer" id="appscontainer">
 				<!--<header ><h3>Listed Apps</h3></header>-->
-				
+
 				@foreach($sidebar as $user)
 					<div class="holder slideout_inner" id="{{$user->user_id}}">
 						<h4 class="section-title" >{{$user->frst_nm . " ".$user->last_nm}} </h4>
-						
+
 						@foreach($posts as $post)
 							@if($post->user->user_id == $user->user_id)
 							<div class="post" data-postid="{{$post->app_id}}">
@@ -37,36 +37,56 @@
 									<img class="icon" src="{{$post->isSet($post->app_icon_path) ? $post->app_icon_path : 'uploads/noimageicon.png' }}" alt="Image" />
 								</div>
 								<div>
-									<p class="title" title="{{strtoupper($post->app_nm)}}"> {{strtoupper($post->app_nm)}}        
+									<p class="title" title="{{strtoupper($post->app_nm)}}"> {{strtoupper($post->app_nm)}}
 									</p>
 									<p class="downloadables">
 										<a class="btn btn-info btn-sm" href="{{$post->app_path}}" {{$post->isUrl($post->app_path) ? 'target="_blank"' : 'download'}} data-name="{{$post->app_path}}">Download</a> |
 										@if($post->isSet($post->app_manual_path))
 											<a class="btn btn-info btn-sm" href="{{$post->app_manual_path}}" data-title="manual" {{$post->isUrl($post->app_manual_path) ? 'target="_blank"' : 'download'}}  data-name="{{$post->app_manual_path}}">Manual</a> |
-										@endif  
+										@endif
 										@if($post->isSet($post->app_readme_path))
 										   <a class="btn btn-info btn-sm" href="{{$post->app_readme_path}}" data-title="readme" {{$post->isUrl($post->app_readme_path) ? 'target="_blank"' : 'download'}} data-name="{{$post->app_readme_path}}">Read Me</a> |
-										@endif                                    
+										@endif
 									</p>
 								</div>
 								<!--<div class="info">
 									Posted on {{$post->created_dt}}
-								</div>	-->				
+								</div>	-->
 							</div>
-							@endif						
+							@endif
 						@endforeach
 					</div>
 				@endforeach
-				
+
 				{{----}}
-				
+
 			</div>
 			</div>
 		</main>
-		
+
 		</div>
 		<script>
         var token = '{{Session::token()}}';
         var url = '{{route('search')}}';
+
+        if(window.screen.availWidth<"1152")
+        {
+          var jumbotron = document.getElementById("top");
+          jumbotron.style.width="760px";
+
+          var holder = document.querySelectorAll(".holder.slideout_inner");
+          for(var t=0;t<holder.length;t++)
+          {
+            holder[t].style.width="760px";
+          }
+        }
+        if(window.screen.availHeight<"920")
+        {
+          var nav = document.getElementById("sidebar");
+          nav.style.overflowY="scroll";
+          nav.style.bottom="0px";
+        }
+
+
     </script>
 @endsection
